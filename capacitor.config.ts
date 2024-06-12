67924fed9a8eeda0a78be3caf4299f80cdac266d
vodu-dmo <danielfelipe.maldonado@outlook.com>
2024-06-11 23:28:19 -0500

    kick starter

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tav.app',
+  appName: 'trav',
+  webDir: 'www'
+};
+
+export default config;
