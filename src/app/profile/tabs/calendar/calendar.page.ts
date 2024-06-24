5d0bf14f28ac5aedeb2fa15c705f9568a34f3379
vodu-dmo <danielfelipe.maldonado@outlook.com>
2024-06-24 10:23:54 -0500

    kick starter

--- src/app/profile/tabs/calendar/calendar.page.ts
+++ src/app/profile/tabs/calendar/calendar.page.ts
@@ -8,7 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./calendar.page.scss'],
 })
 export class CalendarPage {
-
+  selectedDate: Date = new Date(2024, 5, 24); // 24 de junio de 2024
+  ;
   constructor() { }
 
 }
