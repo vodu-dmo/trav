f36f9585fd5330b197d0a4608056cf13e06fff52
vodu-dmo <danielfelipe.maldonado@outlook.com>
2024-06-25 11:30:29 -0500

    kick starter

--- src/app/profile/tabs/calendar/calendar.page.ts
+++ src/app/profile/tabs/calendar/calendar.page.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { CalendarEvent, CalendarView } from 'angular-calendar';
-import { startOfDay, endOfDay } from 'date-fns';
 
 @Component({
   selector: 'app-calendar',
@@ -8,14 +6,7 @@ import { startOfDay, endOfDay } from 'date-fns';
   styleUrls: ['./calendar.page.scss']
 })
 export class CalendarPage {
-  view: CalendarView = CalendarView.Month;
-  viewDate: Date = new Date();
-  events: CalendarEvent[] = [
-    {
-      start: startOfDay(new Date()),
-      end: endOfDay(new Date()),
-      title: 'An event',
-      color: { primary: '#e3bc08', secondary: '#FDF1BA' }
-    }
-  ];
+  constructor() {}
+
+ 
 }
