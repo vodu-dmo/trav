3ce465dd3dcc5562abce7a26eb2a7eeaa3a322c5
vodu-dmo <danielfelipe.maldonado@outlook.com>
2024-06-25 11:46:33 -0500

    kick starter

--- src/app/profile/tabs/calendar/calendar.module.ts
+++ src/app/profile/tabs/calendar/calendar.module.ts
@@ -4,16 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { CalendarPageRoutingModule } from './calendar-routing.module';
 import { CalendarPage } from './calendar.page';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CalendarPageRoutingModule,
-    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
+    CalendarPageRoutingModule
   ],
   declarations: [CalendarPage]
 })
